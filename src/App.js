2f053f4d8f8e9efd2908f06cc38e1e5ee5e95ca3
Conor Garity <conorgarity@Conors-MacBook-Pro.local>
2019-07-30 09:50:42 -0400

    Get Axios Data

--- src/App.js
+++ src/App.js
@@ -12,22 +12,36 @@ class App extends React.Component {
     this.state = {
         genreFilter: '',
         dateFilter: '',
-        ratingFilter: ''
-
+        ratingFilter: '',
+        dateEndFilter: '',
+        recommendations: []
     
     }
+    this.genreArrarys = {
+      horror: [10695,10944,1694,42023,45028,48303,61546,75405,75804,75930,8195,83059,8711,89585],
+      action: [10673,10702,11804,11828,1192487,1365,1568,2125,2653,43040,43048,4344,46576,75418,76501,77232,788212,801362,852490,899,9584],
+      comedy: [1009,10256,10375,105,10778,11559,11755,1208951,1333288,1402,1747,17648,2030,2700,31694,3300,34157,3519,3996,4058,4195,43040,4426,4906,52104,52140,52847,5286,5475,5610,56174,58905,59169,61132,61330,6197,63092,63115,6548,711366,7120,72407,7539,77599,77907,78163,78655,79871,7992,852492,869,89585,9302,9434,9702,9736]
+    }
+
   }
 
   changeGenreFilter = (newFilter) =>{
     this.setState({
-      genreFilter: newFilter
+      genreFilter: this.genreArrarys[newFilter]
     });
 
   }
 
   changeDateFilter = (newFilter) =>{
+    let newEndDateFilter = '2019';
+
+    if(parseInt(newFilter)+20 < 2019){
+      newEndDateFilter = parseInt(newFilter) + 20
+    }
+
     this.setState({
-      dateFilter: newFilter
+      dateFilter: newFilter,
+      dateEndFilter: newEndDateFilter
     });
 
   }
@@ -39,8 +53,38 @@ class App extends React.Component {
 
   }
 
-  search = () => {
+  search = async(e) => {
+    e.preventDefault();
     console.log("Search activated");
+    const REACT_APP_API_KEY = ' ';
+
+    try{
+      const apiUrl = `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get:new7-!${this.state.dateFilter},${this.state.dateEndFilter}-!0,5-!${this.state.ratingFilter},10-!${this.state.genreFilter}-!Any-!Any-!Any-!gt100-!{downloadable}&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and`;
+
+         
+      // const apiResponse = await Axios.get(apiUrl)
+      // .header("X-RAPIDAPI-KEY", REACT_APP_API_KEY); 
+      
+      const apiResponse = await Axios({
+        url: apiUrl,
+        method: 'GET',
+        headers: {"X-RapidAPI-Key": REACT_APP_API_KEY,
+                  "X-RapidAPI-Host":"unogs-unogs-v1.p.rapidapi.com"}
+      })
+
+      const movieData = apiResponse.data;
+      console.log(apiResponse);
+      console.log(movieData.ITEMS);
+
+      if(apiResponse.status === 200){
+        this.setState(prevState => ({
+          recommendations: movieData.ITEMS
+        }));
+      } 
+    }catch{
+
+    }
+
   }
 
   render(){
